6bb6475b2b9567e8f9c713aa982c4150ef1115c5
IvanFNAF3 <126335680+IvanFNAF3@users.noreply.github.com>
2023-02-25 19:15:55 +0300

    Update Player.cs

--- Git Project/Assets/Scripts/Player.cs
+++ Git Project/Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    string a = "Hello World!";
     void Start()
     {
         Debug.Log("Hello! This is Test for GitHub.com!");
         Debug.Log("Hello! This sentence was made by GitHub.com!");
+        Debug.Log(a);
     }
 }
