86e0b1948da13f1fc6318d61280b87273c90b441
IvanFNAF3 <126335680+IvanFNAF3@users.noreply.github.com>
2023-02-25 19:10:54 +0300

    Second Update
    added a new Script 'Player';

---
+++ Git Project/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello! This is Test for GitHub.com!");
+    }
+}
